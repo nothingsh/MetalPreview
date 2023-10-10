ab24c4b751cd4656aad865bda37d9587df5756c9
Wynn Zhang <sn773636227@outlook.com>
2023-10-10 16:35:41 +0800

    Initial Commit

---
+++ MetalPreview/MetalPreview.h
@@ -0,0 +1,18 @@
+//
+//  MetalPreview.h
+//  MetalPreview
+//
+//  Created by Wynn Zhang on 10/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetalPreview.
+FOUNDATION_EXPORT double MetalPreviewVersionNumber;
+
+//! Project version string for MetalPreview.
+FOUNDATION_EXPORT const unsigned char MetalPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalPreview/PublicHeader.h>
+
+
